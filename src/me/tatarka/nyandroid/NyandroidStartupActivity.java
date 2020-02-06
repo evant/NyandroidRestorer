e08c51f8b4f57404fdf83f780f076b36dbe05dde
Evan Tatarka <evan@tatarka.me>
2020-02-06 00:08:32 -0500

    Faster loading

--- src/me/tatarka/nyandroid/NyandroidStartupActivity.java
+++ src/me/tatarka/nyandroid/NyandroidStartupActivity.java
@@ -7,19 +7,25 @@ import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
-public class NyandroidStartupActivity implements StartupActivity {
+public class NyandroidStartupActivity implements StartupActivity, StartupActivity.DumbAware {
     private static final String ICON_NYANDROID = "/file-icons/logcat.png";
 
     @Override
     public void runActivity(@NotNull Project project) {
         Icon icon = IconLoader.getIcon(ICON_NYANDROID);
-        ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
-        ToolWindow toolWindow = toolWindowManager.getToolWindow("Logcat");
+        ToolWindow toolWindow = logcatToolWindow(project);
         if (toolWindow != null) {
             ApplicationManager.getApplication().invokeLater(() -> toolWindow.setIcon(icon));
         }
+    }
+
+    @Nullable
+    private static ToolWindow logcatToolWindow(Project project) {
+        ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+        return toolWindowManager.getToolWindow("Logcat");
     }
 }
